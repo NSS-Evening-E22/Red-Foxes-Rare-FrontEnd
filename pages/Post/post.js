4cbb5e5841b92526e8a045f22a63528b8f24f5fc
ahamm <ahammer753@gmail.com>
2023-09-18 21:13:29 -0500

    created a post folder and started post form

--- pages/Post/post.js
+++ pages/Post/post.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { Button } from 'react-bootstrap';
-import { getPosts } from '../utils/data/PostData';
-import { useAuth } from '../utils/context/authContext';
-import PostCard from '../components/PostCard';
+import { getPosts } from '../../utils/data/PostData';
+import { useAuth } from '../../utils/context/authContext';
+import PostCard from '../../components/PostCard';
 
 function Home() {
   const [posts, setPosts] = useState([]);
