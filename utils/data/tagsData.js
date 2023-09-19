0bb9654921382ff8192b9f7d582500e539c6238b
Maddi Cook <maddihcook@gmail.com>
2023-09-18 20:52:43 -0500

    add and update tags endpoints

---
+++ utils/data/tagsData.js
@@ -0,0 +1,34 @@
+const dbUrl = 'https://localhost:7033';
+
+const getTags = () => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/tags`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        resolve(Object.values(data));
+      } else {
+        resolve([]);
+      }
+    })
+    .catch(reject);
+});
+
+const updateTags = (payload) => new Promise((resolve, reject) => {
+  fetch(`${dbUrl}/books/${payload.firebaseKey}.json`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+export { getTags, updateTags };
