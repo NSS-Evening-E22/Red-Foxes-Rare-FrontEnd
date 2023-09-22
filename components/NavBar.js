acdb3a1ba732497cc617397012294f96d1bf08f2
Sheryl Boles <sheryl.boles@gmail.com>
2023-09-21 21:04:07 -0400

    trying to do something

--- components/NavBar.js
+++ components/NavBar.js
@@ -29,7 +29,7 @@ export default function NavBar() {
             <Link passHref href="/createTag">
               <Nav.Link>Create Tag</Nav.Link>
             </Link>
-            <Link passHref href="/manage user">
+            <Link passHref href="/viewusers">
               <Nav.Link>View Users</Nav.Link>
             </Link>
             <Button variant="danger" onClick={signOut}>
