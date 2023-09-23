f2ffac9248218cf0fbf670300e1f4ff4842f8ac8
Jasper Baltz <nickolas.baltz@gmail.com>
2023-09-23 14:04:28 -0500

    eslint errors fixed

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Button } from 'react-bootstrap';
 import { useState, useEffect } from 'react';
 import RegisterForm from '../components/RegisterForm';
